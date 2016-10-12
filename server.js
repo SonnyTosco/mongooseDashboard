030c542c31846d7a5637b60f6e80e5c05c67e7b9
Sonny Tosco <sonny.tosco@gmail.com>
2016-10-12 12:31:32 -0700

    files setup

---
+++ server.js
@@ -0,0 +1,47 @@
+var   express     = require('express'),
+      bodyParser  = require('body-parser'),
+      path        = require('path'),
+      io          = require('socket.io')
+      app         = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+//Routes
+
+app.get('/', function(req, res) {
+  //query to display all species
+  res.render('index');
+})
+
+app.get('/species/:id', function(req, res) {
+  //query to show that particular mongoose
+  res.render('show');
+})
+
+app.get('/species/new', function(req, res) {
+  //want to make sure that the create view is hit
+  res.render('create');
+})
+
+app.post('/species', function(req, res) {
+  //query to insert a new instance of mongoose into the db
+  res.redirect('/');
+})
+
+app.get('/species/:id/edit', function(req, res){
+  //query that specific mongoose that exists in the db
+  res.render('edit');
+})
+
+app.post('/species/:id', function(req, res){
+  //query that stores the updated information for that instance
+  res.redirect('/');
+})
+
+app.post('/species/:id/destroy', function(req, res){
+  //query that removes that specific document
+  res.redirect('/')
+})
